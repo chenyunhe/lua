3552a411fcb918063e767b578675ea25978243a5
chenyunhe <3177856456@qq.com>
2017-07-04 11:41:27 +0800

    Update date.lua

--- date.lua
+++ date.lua
@@ -1,3 +1,4 @@
  local date = os.date('%m%d')
  local date = os.date('%Y%m%d')
  local date = os.date('%Y-%m-%d %H:%M:%S',1499097600)
+ local t=os.time({day=4, month=7, year=2017, hour=0, minute=0, second=0})
